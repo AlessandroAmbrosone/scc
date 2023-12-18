45fb2045d73e9cf51332149655dd591967d099fd
lessandro00 <a.ambrosone8@studenti.unisa.it>
2023-12-18 16:51:05 +0100

    nani

---
+++ esame/load_data.py
@@ -0,0 +1,127 @@
+import pandas as pd
+from pandas.api.types import CategoricalDtype
+from category_encoders import MEstimateEncoder
+
+def load_and_preprocess_data(args):
+    # Read data
+    df_train = pd.read_csv(args + "train.csv", index_col="Id")
+    df_test = pd.read_csv(args + "test.csv", index_col="Id")
+    # Merge the splits so we can process them together
+    df = pd.concat([df_train, df_test])
+    # Preprocessing
+    df = clean(df)
+    df = encode(df)
+    df = impute(df)
+    # Reform splits
+    df_train = df.loc[df_train.index, :]
+    df_test = df.loc[df_test.index, :]
+    return df_train, df_test 
+
+def clean(df):
+    df["Exterior2nd"] = df["Exterior2nd"].replace({"Brk Cmn": "BrkComm"})
+    # Some values of GarageYrBlt are corrupt, so we'll replace them
+    # with the year the house was built
+    df["GarageYrBlt"] = df["GarageYrBlt"].where(df.GarageYrBlt <= 2010, df.YearBuilt)
+    # Names beginning with numbers are awkward to work with
+    df.rename(columns={
+        "1stFlrSF": "FirstFlrSF",
+        "2ndFlrSF": "SecondFlrSF",
+        "3SsnPorch": "Threeseasonporch",
+    }, inplace=True,
+    )
+    return df
+
+# The numeric features are already encoded correctly (`float` for
+# continuous, `int` for discrete), but the categoricals we'll need to
+# do ourselves. Note in particular, that the `MSSubClass` feature is
+# read as an `int` type, but is actually a (nominative) categorical.
+
+# The nominative (unordered) categorical features
+features_nom = ["MSSubClass", "MSZoning", "Street", "Alley", "LandContour", "LotConfig", "Neighborhood", "Condition1", "Condition2", "BldgType", "HouseStyle", "RoofStyle", "RoofMatl", "Exterior1st", "Exterior2nd", "MasVnrType", "Foundation", "Heating", "CentralAir", "GarageType", "MiscFeature", "SaleType", "SaleCondition"]
+
+# Pandas calls the categories "levels"
+five_levels = ["Po", "Fa", "TA", "Gd", "Ex"]
+ten_levels = list(range(10))
+
+ordered_levels = {
+    "OverallQual": ten_levels,
+    "OverallCond": ten_levels,
+    "ExterQual": five_levels,
+    "ExterCond": five_levels,
+    "BsmtQual": five_levels,
+    "BsmtCond": five_levels,
+    "HeatingQC": five_levels,
+    "KitchenQual": five_levels,
+    "FireplaceQu": five_levels,
+    "GarageQual": five_levels,
+    "GarageCond": five_levels,
+    "PoolQC": five_levels,
+    "LotShape": ["Reg", "IR1", "IR2", "IR3"],
+    "LandSlope": ["Sev", "Mod", "Gtl"],
+    "BsmtExposure": ["No", "Mn", "Av", "Gd"],
+    "BsmtFinType1": ["Unf", "LwQ", "Rec", "BLQ", "ALQ", "GLQ"],
+    "BsmtFinType2": ["Unf", "LwQ", "Rec", "BLQ", "ALQ", "GLQ"],
+    "Functional": ["Sal", "Sev", "Maj1", "Maj2", "Mod", "Min2", "Min1", "Typ"],
+    "GarageFinish": ["Unf", "RFn", "Fin"],
+    "PavedDrive": ["N", "P", "Y"],
+    "Utilities": ["NoSeWa", "NoSewr", "AllPub"],
+    "CentralAir": ["N", "Y"],
+    "Electrical": ["Mix", "FuseP", "FuseF", "FuseA", "SBrkr"],
+    "Fence": ["MnWw", "GdWo", "MnPrv", "GdPrv"],
+}
+
+# Add a None level for missing values
+ordered_levels = {key: ["None"] + value for key, value in
+                  ordered_levels.items()}
+
+
+def encode(df):
+    # Nominal categories
+    for name in features_nom:
+        df[name] = df[name].astype("category")
+        # Add a None category for missing values
+        if "None" not in df[name].cat.categories:
+            df[name].cat.add_categories("None", inplace=True)
+    # Ordinal categories
+    for name, levels in ordered_levels.items():
+        df[name] = df[name].astype(CategoricalDtype(levels,
+                                                    ordered=True))
+    return df
+
+def impute(df):
+    for name in df.select_dtypes("number"):
+        df[name] = df[name].fillna(0)
+    for name in df.select_dtypes("category"):
+        df[name] = df[name].fillna("None")
+    return df
+
+y_train = df_train['SalePrice']
+y_test = df_test['SalePrice']
+x_train = df_train.drop('SalePrice', axis=1)
+x_test = df_test.drop('SalePrice', axis=1)
+
+x_train, x_test, y_train, y_test = x_train.to_numpy(), x_test.to_numpy(), y_train.to_numpy(), y_test.to_numpy()
+data = {'x_train': x_train.tolist(), 'y_train': y_train.tolist(), 'x_test': x_test.tolist(), 'y_test': y_test.tolist()}
+
+data_json = json.dumps(data)
+# Saves the json object into a file
+with open(args.data, 'w') as out_file:
+    json.dump(data_json, out_file)
+
+df_train, df_test = load_and_preprocess_data()
+
+if __name__ == '__main__':
+    
+    # This component does not receive any input
+    # it only outpus one artifact which is `data`.
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--data', type=str)
+    
+    args = parser.parse_args()
+    
+    # Creating the directory where the output file will be created 
+    # (the directory may or may not exist).
+    Path(args.data).parent.mkdir(parents=True, exist_ok=True)
+
+    load_and_preprocess_data(args)
+
