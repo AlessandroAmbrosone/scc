de4cdc2e5bacb5cb7c70b5e502e5929980093a23
lessandro00 <a.ambrosone8@studenti.unisa.it>
2023-12-18 17:14:08 +0100

    ops

--- esame/load_data/load_data.py
+++ esame/load_data/load_data.py
@@ -1,6 +1,7 @@
 import pandas as pd
 from pandas.api.types import CategoricalDtype
 from category_encoders import MEstimateEncoder
+import json
 
 def load_and_preprocess_data(args):
     # Read data
